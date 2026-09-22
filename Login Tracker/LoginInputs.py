d3a5c954dd67885fa198a951747ff21106f5658f
collin2077 <collin@edunn.net>
2026-09-21 20:39:26 -0600

    Create Statmens and need to make a while true loop

--- Login Tracker/LoginInputs.py
+++ Login Tracker/LoginInputs.py
@@ -23,22 +23,33 @@ domaindict = {
 
 print_dict_values = domaindict.values() #Print origanal dictionary values to make sure they are correct
 
-print(print_dict_values)
+#print(print_dict_values)
 
-def user_input_for_login(): #collecting Last Login from user input and returning it to be used in the update_last_login function
-    domain_input = input("Enter the domain name: ")
-    domain_login = input("Enter the last login date and time (YYYY-MM-DD) : " )
-    return domain_input, domain_login
+def user_input_for_domain_update(): #collecting Last Login from user input and returning it to be used in the update_last_login function
+    input_domain_selection = input("Enter the domain name: ")
+    return input_domain_selection
 
-user_domain, user_login = user_input_for_login() #Run user input function to get domain and login date
+def user_input_for_login():
+    input_user_updates_domain_login = input("Enter the last login date and time (YYYY-MM-DD) : " )
+    return input_user_updates_domain_login
+
+user_domain_select = user_input_for_domain_update() #Run user input function to get domain
+
+
+print("MAKE THIS A WHILE TRUE LOOP")
+# if user_domain_select in domaindict.values(): #Check if the user input domain is in the dictionary and print a message
+#     print(f"{user_domain_select} is in the dictionary.")
+# else:
+#     print(f"{user_domain_select} is not in the dictionary.")
+#     user_domain_select = user_input_for_domain_update() #Run user input function to get domain again
 
 def update_last_login(domain):
     if domain in domaindict:
-        domaindict[domain] = user_login  # Update with the user-provided login date and time
+        domaindict[domain] = user_input_for_login()  # Update with the user-provided login date and time
         print(f"Last login for {domain} updated.")
     else:
         print(f"{domain} not found in the dictionary.")
 
-update_last_login("domain1")  # Example usage
+update_last_login(user_domain_select)# Example usage
 
-print(print_dict_values) 
+print(print_dict_values)
