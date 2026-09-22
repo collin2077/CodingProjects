e892718b678f3e2900de4430f6717214e7f9348d
collin2077 <collin@edunn.net>
2026-09-21 20:11:11 -0600

    Created Functions for User Updates to Login

--- Login Tracker/LoginInputs.py
+++ Login Tracker/LoginInputs.py
@@ -1,35 +1,44 @@
 #domain = input("Enter the domain name: ")
 
-def get_domain():
-    while True:
-        if domain == str(domain):
-            print(domain)
-            break
-        else:
-            print("Try again")
-            domain = input("Enter the domain name: ")
-            continue
+# def get_domain():
+#     while True:
+#         if domain == str(domain):
+#             print(domain)
+#             break
+#         else:
+#             print("Try again")
+#             domain = input("Enter the domain name: ")
+#             continue
 
-domaindict = {
+
+# Line 15 is the dictionary for Domain Name and Base Login Date
+domaindict = { 
     "domain1" : "Tower",
-    "last login1" : "2024-06-01 12:00:00",
+    "last login1" : "2024-06-01",
     #"domain2" : "Waypoint",
-    #"last login2" : "2024-06-01 12:00:00",
+    #"last login2" : "2024-06-01 ",
     #"domain3" : "Archer",
-    #"last login3" : "2024-06-01 12:00:00",  
+    #"last login3" : "2024-06-01",  
 }
 
-x = domaindict.values()
+print_dict_values = domaindict.values() #Print origanal dictionary values to make sure they are correct
 
-print(x)
+print(print_dict_values)
+
+def user_input_for_login(): #collecting Last Login from user input and returning it to be used in the update_last_login function
+    domain_input = input("Enter the domain name: ")
+    domain_login = input("Enter the last login date and time (YYYY-MM-DD) : " )
+    return domain_input, domain_login
+
+user_domain, user_login = user_input_for_login() #Run user input function to get domain and login date
 
 def update_last_login(domain):
     if domain in domaindict:
-        domaindict[domain] = "2025-12-01"  # Update with the current date and time
+        domaindict[domain] = user_login  # Update with the user-provided login date and time
         print(f"Last login for {domain} updated.")
     else:
         print(f"{domain} not found in the dictionary.")
 
 update_last_login("domain1")  # Example usage
 
-print(x) 
+print(print_dict_values) 
