d1f6fb9321bbf278966a61e7e8183290973eda6f
SnowSkywalkerIce <slicerdan@outlook.com>
2026-09-21 19:26:23 -0600

    uhhhh

--- Coding Files/InputTesting.py
+++ Coding Files/InputTesting.py
@@ -1,3 +1,15 @@
 #Testing Inputs
-input1 = input("Enter a number: ")
-print(input1)
+#input1 = input("Enter a number: ")
+
+
+
+def script():
+    # program code here...
+    input1 = input("Enter a number: ")
+    
+    restart = input("Would you like to restart this program?")
+    if restart == "yes" or restart == "y":
+        script()
+    if restart == "n" or restart == "no":
+        print ("Script terminating. Goodbye.")
+script()        
